82c8de9db8a9e82a45a2d1f6b5f9d51492d57a04
Delisa Mason <iskanamagus@gmail.com>
2015-03-03 13:33:39 -0800

    Lost my toucan. Cleanup stuff that didn't work
    This reverts commit a1cbb7d55f6f0cdf8e2cc3bc01004b30c304486f.

--- lib/travis_formatter.rb
+++ lib/travis_formatter.rb
@@ -2,28 +2,31 @@
 class TravisFormatter < XCPretty::Simple
 
   def open_fold(text)
-    return EMPTY if text == @open_fold
+    return if text == @open_fold
     close_fold(@open_fold) if @open_fold
+    puts "travis_fold:start:#{text}\r"
     @open_fold = text
-    "travis_fold:start:#{text}\r\n"
   end
 
   def close_fold(text)
+    puts "travis_fold:end:#{text}\r"
     @open_fold = nil
-    "travis_fold:end:#{text}\r"
   end
 
   def format_build_target(target, project, configuration)
-    open_fold("Build-#{scrub(project)}") + super
+    open_fold("Build-#{scrub(project)}")
+    super
   end
 
   def format_clean_target(project, target, configuration)
-    open_fold("Clean") + super
+    open_fold("Clean")
+    super
   end
 
   def format_test_run_started(name)
     run = scrub(name).split(".").first
-    open_fold("Tests-#{run}") + super
+    open_fold("Tests-#{run}")
+    super
   end
 
   def scrub(text)
