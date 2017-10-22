a8c85556cae806cb3e47f0e3fd443970d1645159
Mark Carver <mark.carver@me.com>
2017-10-21 22:51:25 -0500

    Major update
    This contains a major refactoring to take into account several things that have changed over the years, especially with Travis and Xcode 9 output.

--- lib/travis_formatter.rb
+++ lib/travis_formatter.rb
@@ -1,46 +1,94 @@
 
 class TravisFormatter < XCPretty::Simple
 
-  def open_fold(text)
-    return if text == @open_fold
-    close_fold(@open_fold) if @open_fold
-    print "travis_fold:start:#{text}\r"
-    @open_fold = text
-  end
-
-  def close_fold(text)
-    print "travis_fold:end:#{text}\r"
-    @open_fold = nil
-  end
-
-  def format_build_target(target, project, configuration)
-    open_fold("Build")
+  def initialize (use_unicode, colorize)
     super
+    @currentGroup = nil
+    @errors = []
+    @seenGroups = {}
+    @travis = ENV['TRAVIS'].to_s == 'true'
+    @warnings = []
+    at_exit do
+      # Ensure the last opened fold is closed.
+      close_fold()
+
+      # Print out any warnings.
+      if !@warnings.compact.empty?
+        open_fold("Warnings")
+        STDOUT.puts @warnings.compact.join("\n")
+        close_fold()
+      end
+
+      # Print out any errors.
+      if !@errors.compact.empty?
+        open_fold("Errors")
+        STDOUT.puts @errors.compact.join("\n")
+        exit(1)
+      end
+    end
   end
 
-  def format_analyze_target(target, project, configuration)
-    open_fold("Analyze")
-    super
+  def format_group(group, track = true)
+    group = group.downcase.gsub(/[^a-z\d\-_.]+/, '-').gsub(/-$/, '')
+    i = 1
+    parts = group.split('.')
+    if parts.last =~ /^\d+$/
+      last = parts.pop()
+      i = last ? last.to_i : 1
+      group = parts.join('.')
+    end
+
+    if track && @currentGroup != "#{group}.#{i}" && @seenGroups.has_key?(group)
+      i = @seenGroups[group] + 1
+    end
+    
+    @seenGroups[group] = i
+    "#{group}.#{i}"
   end
 
-  def format_clean_target(target, project, configuration)
-    open_fold("Clean")
-    super
+  def close_fold()
+    return if not @travis or @currentGroup == nil
+    STDOUT.puts "travis_fold:end:#{@currentGroup}\n"
+    @currentGroup = nil
   end
 
-  def format_test_run_started(name)
-    open_fold("Tests-#{scrub(name)}")
-    super
+  def open_fold(group, track = true)
+    description = group
+    group = format_group(group, track)
+    return if @currentGroup == group or not @travis
+    close_fold() if @currentGroup != nil
+    @currentGroup = group
+    STDOUT.puts "travis_fold:start:#{group}\033[33;1m#{description}\033[0m\n"
   end
 
-  def format_test_run_finished(name, time)
-    close_fold("Tests-#{scrub(name)}")
-    super
-  end
+  # Analyze.
+  def format_analyze(file_name, file_path);                                 open_fold("Analyze"); super; end
+  def format_analyze_target(target, project, configuration);                open_fold("Analyze"); super; end
 
-  def scrub(text)
-    text.gsub(/\s/,"_").split(".").first
-  end
+  # Clean.
+  def format_clean(project, target, configuration);                         open_fold("Clean"); super; end
+  def format_clean_target(target, project, configuration);                  open_fold("Clean"); super; end
+  def format_clean_remove;                                                  open_fold("Clean"); super; end
+
+  # Compile.
+  def format_compile(file_name, file_path);                                 open_fold("Compile"); super; end
+
+  # Test.
+  def format_test_run_started(name);                                        open_fold("Test"); super; end
+  def format_test_suite_started(name);                                      open_fold("Test"); super; end
+  def format_failing_test(suite, test, reason, file_path);                  @errors.push(super); super; end
+  def format_test_summary(message, failures_per_suite);                     @errors.concat(failures_per_suite.values); super; end
+
+  # Errors and warnings.
+  def format_compile_error(file_name, file_path, reason, line, cursor);     @errors.push(super); ""; end
+  def format_error(message);                                                @errors.push(super); ""; end
+  def format_file_missing_error(error, file_path);                          @errors.push(super); ""; end
+  def format_ld_warning(message);                                           @warnings.push(super); ""; end
+  def format_undefined_symbols(message, symbol, reference);                 @warnings.push(super); ""; end
+  def format_duplicate_symbols(message, file_paths);                        @warnings.push(super); ""; end
+  def format_warning(message);                                              @warnings.push(super); ""; end
+  def format_compile_warning(file_name, file_path, reason, line, cursor);   @warnings.push(super); ""; end
+
 end
 
 TravisFormatter
